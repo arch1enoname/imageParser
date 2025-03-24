cb062f843e7e9e72194ef33b6f8f1742c847fcee
arthur <artur132003@gmail.com>
2025-03-24 13:25:57 +0400

    Rename project

--- src/main/java/com/ss/imageParser/ImageParserApplication.java
+++ src/main/java/com/ss/imageParser/ImageParserApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToptApplication {
+public class ImageParserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ToptApplication.class, args);
+        SpringApplication.run(ImageParserApplication.class, args);
     }
 }
